ddf5600e3611100f26ef22cff40dfc3ddb94234e
juanleguizamon <jleguizamon02@ufl.edu>
2022-03-24 20:25:30 -0500

    updated code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # m6-hw9-leguizamon-juan
 Adv Web 2 HW9
+
+# Issues
+I didn't run into many issues with this assignment. The only thing I did run into was running tests using city names that have more than one word, ex. Kansas City. The API didn't seem to want to recognize those.
+
+# Resources
+I didn't use any outside resources, I mainly depending on the class notes and previous assignments
---
+++ index.js
@@ -0,0 +1,69 @@
+const key = '4d5fe15463f8f9b69807c76e7a411130';
+const noResultMessage = [
+    "Location not found",
+    "Incorrect location",
+];
+
+var button = document.querySelector('button');
+var inputElement = document.getElementById('city');
+var returnObj = document.querySelector('.returnObject');
+var city = document.querySelector('.city');
+var icon = document.querySelector('.icon');
+var description = document.querySelector('.description');
+var currentTemp = document.querySelector('.current');
+var feelsLikeTemp = document.querySelector('.feels');
+
+var tof = 0;
+
+button.onclick = function() {
+    if(tof == 0){
+        tof = 1;
+    }else {
+        tof = 0;
+    }
+
+    var inputQuery = inputElement.value.trim();
+    var noSpaceQuery = inputQuery.split(' ').join('');
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q='+noSpaceQuery+',US&limit=3&appid='+key) // make the request
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(res) {
+        pullLatLong(res);
+    })
+}
+
+function pullLatLong(locationObj) {
+    if(locationObj.length === 0){
+        clearHTML();
+        city.textContent = noResultMessage[tof];
+        return;
+    }
+    var lat = locationObj[0].lat;
+    var long = locationObj[0].lon;
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid='+key+'&units=imperial') // make the request
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(res) {
+        displayWeather(res);
+    })
+}
+
+function displayWeather(weatherObj) {
+    returnObj.style.background = 'rgba(186, 161, 161, 0.25)';
+    city.textContent = weatherObj.name + ', ' + weatherObj.sys.country;
+    icon.innerHTML = '<img src="http://openweathermap.org/img/wn/'+weatherObj.weather[0].icon+'@2x.png"></img>';
+    description.textContent = weatherObj.weather[0].description;
+    currentTemp.textContent = 'Current: ' + weatherObj.main.temp + ' \xB0F';
+    feelsLikeTemp.textContent = 'Feels Like: ' + weatherObj.main.feels_like + ' \xB0F';
+}
+
+function clearHTML() {
+    returnObj.style.background = '';
+    city.textContent = '';
+    icon.innerHTML = '';
+    description.innerHTML = '';
+    currentTemp.textContent = '';
+    feelsLikeTemp.textContent = '';
+}
